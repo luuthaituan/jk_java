722ed36add26ce17388ae48c2a2fb8f5a5e85acf
luuthaituan <luuthaituan08091999@gmail.com>
2023-09-12 14:36:15 +0700

    second commit

--- jenkins/src/jenkins/Jenkins.java
+++ jenkins/src/jenkins/Jenkins.java
@@ -16,6 +16,7 @@ public class Jenkins {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello");
+        System.out.println("I am testing my Jenkins");
     }
     
 }
